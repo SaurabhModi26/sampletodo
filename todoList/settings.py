5e596bf3825ab46a380b38d9b0ef46217dd345d3
Saurabh <modi.2@iitj.ac.in>
2021-03-27 15:51:21 +0530

    fixed static issues

--- todoList/settings.py
+++ todoList/settings.py
@@ -123,5 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
