68b975a4687073a8608a0498bbe1d4575261c1f6
Saurabh <modi.2@iitj.ac.in>
2021-03-27 16:02:56 +0530

    define static root

--- todoList/settings.py
+++ todoList/settings.py
@@ -125,5 +125,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
